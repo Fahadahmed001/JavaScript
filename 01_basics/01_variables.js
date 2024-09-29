0bf8e5fb975001017776618efd46f1010d4d7753
Fahad Ahmed <118359928+Fahadahmed001@users.noreply.github.com>
2024-09-29 19:41:09 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "Fahad@google.com"
+var accountPassword = "112233"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2   //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
